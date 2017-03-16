6baa6f5ba66ec59215a5a35527baa0e151448e7d
Ricardo Bocchi <ricardo@mobilemind.com.br>
2017-03-16 10:04:52 -0300

    ios bug fix add icon button

--- view-util.ios.js
+++ view-util.ios.js
@@ -12,30 +12,50 @@ global.moduleMerge(common, exports);
 
 exports.addViewIcon = function(view, position, iconName){
     var icon = UIImage.imageNamed(iconName)
-    var button = view.ios
-    button.titleLabel.textAlignment = NSTextAlignmentCenter
-    button.setImageForState(icon, UIControlStateNormal)
-    
-    if(position == 'left'){
-		button.transform = CGAffineTransformMakeScale(1.0, -1.0);
-		button.titleLabel.transform = CGAffineTransformMakeScale(1.0, -1.0);
-		button.imageView.transform = CGAffineTransformMakeScale(1.0, -1.0);    		    
-	    button.titleEdgeInsets = UIEdgeInsetsMake(0, 10, 0, 0);    	
-    }else if(position == 'right'){    	
-		button.transform = CGAffineTransformMakeScale(-1.0, 1.0);
-		button.titleLabel.transform = CGAffineTransformMakeScale(-1.0, 1.0);
-		button.imageView.transform = CGAffineTransformMakeScale(-1.0, 1.0);    	
-		button.titleEdgeInsets = UIEdgeInsetsMake(0, 0, 0, 10);    	
-	}
+
+    if(view.ios instanceof UIButton){
+      var button = view.ios
+      button.titleLabel.textAlignment = NSTextAlignmentCenter
+      button.setImageForState(icon, UIControlStateNormal)
+      if(position == 'left'){
+        button.transform = CGAffineTransformMakeScale(1.0, -1.0);
+        button.titleLabel.transform = CGAffineTransformMakeScale(1.0, -1.0);
+        button.imageView.transform = CGAffineTransformMakeScale(1.0, -1.0);
+        button.titleEdgeInsets = UIEdgeInsetsMake(0, 10, 0, 0);
+      }else if(position == 'right'){
+        button.transform = CGAffineTransformMakeScale(-1.0, 1.0);
+        button.titleLabel.transform = CGAffineTransformMakeScale(-1.0, 1.0);
+        button.imageView.transform = CGAffineTransformMakeScale(-1.0, 1.0);
+        button.titleEdgeInsets = UIEdgeInsetsMake(0, 0, 0, 10);
+      }
+    }else if(view.ios instanceof UILabel){
+
+      var label = view.ios
+      var attachment = NSTextAttachment.alloc().init()
+      attachment.image = icon
+
+      attachmentString = NSAttributedString.attributedStringWithAttachment(attachment)
+
+      var textAttr = NSMutableAttributedString.alloc().initWithString(label.text)
+      textAttr.appendAttributedString(attachmentString)
+
+      if(position == 'left'){
+        label.textAlignment = NSTextAlignmentLeft
+      }else {
+        label.textAlignment = NSTextAlignmentRight
+      }
+
+      label.attributedText = textAttr
+    }
 }
 
 exports.buttonRoundedAll = function(view, hexaColor, raduis){
   buttonRounded(view, hexaColor, raduis, raduis, raduis, raduis)
 }
 
-function buttonRounded(view, hexaColor, top_left, top_right, bottom_right, bottom_left){    
+function buttonRounded(view, hexaColor, top_left, top_right, bottom_right, bottom_left){
     view.ios.clipsToBounds = true;
-    view.ios.layer.cornerRadius = top_right;   
+    view.ios.layer.cornerRadius = top_right;
 }
 
 exports.buttonRounded = buttonRounded;
@@ -45,7 +65,7 @@ exports.addTextChangeListener = function(textView, textChangeCallback){
 	MyChangeListener = NSObject.extend({
 		onChange: function(textField){
 			textChangeCallback(textField.text)
-		}		
+		}
 	}, {
 	    name: "MyChangeListener",
 	    exposedMethods: {
@@ -53,7 +73,7 @@ exports.addTextChangeListener = function(textView, textChangeCallback){
 	        onChange: { returns: interop.types.void, params: [ UITextField ] }
 	    }
 	})
-	
+
 	var textWatcher = new MyChangeListener()
     textView.ios.addTargetActionForControlEvents(textWatcher, "onChange", UIControlEventEditingChanged)
 
@@ -72,10 +92,10 @@ exports.addKeyboardChangeListener = function(view, onKeyboardOpenCallback, onKey
 	MyKeyboardListener = NSObject.extend({
 		onShow: function(){
 			onKeyboardOpenCallback()
-		},		
+		},
 		onHide: function(){
 			onKeyboardCloseCallback()
-		}		
+		}
 	}, {
 	    name: "MyKeyboardListener",
 	    exposedMethods: {
@@ -98,7 +118,7 @@ exports.keyboardHidden = function(){
 
 exports.keyboardShow = function(){
 	var controller = frameModule.topmost().ios.controller;
-	controller.view.endEditing(true);	
+	controller.view.endEditing(true);
 }
 
 function removeKeyboardChangeListener(){
@@ -108,13 +128,13 @@ exports.removeKeyboardChangeListener = removeKeyboardChangeListener;
 
 exports.getExtraKey = function(key){
 
-  return undefined    
+  return undefined
 }
 
 exports.transparentNav = function() {
- 
-} 
- 
+
+}
+
 exports.getStatusBarHeight = function() {
     return 0;
 }
@@ -139,18 +159,18 @@ exports.getDensityResources = function(){
 
    	var width = platform.screen.mainScreen.widthPixels / platform.screen.mainScreen.scale
     var height = platform.screen.mainScreen.heightPixels / platform.screen.mainScreen.scale
-    
+
     //http://ios-resolution.com/
     switch(width){
       case 414: // IPhone 6 Plus
-        return common.DensityResources.HIGH        
+        return common.DensityResources.HIGH
       case 1242: // IPhone 6 Plus Retina
         return common.DensityResources.XXXHIGH
       case 375: // IPhone 6
         return common.DensityResources.HIGH
       case 750: // IPhone 6 Retina
         return common.DensityResources.XXHIGH
-      case 320: 
+      case 320:
         if(height == 568) // iPhone 5, 5c, 5s, iPod touch 5th gen
           return common.DensityResources.HIGH
         else if(height == 480) // iPhone 1st, 3G, 3GS, 4, 3S, 5, iPod touch 1st-4th gen
@@ -163,8 +183,8 @@ exports.getDensityResources = function(){
       case 768: // iPad (1st gen, 2, 3rd gen, 4th gen) - iPad mini
         return common.DensityResources.HIGH
       case 1536: // iPad (1st gen, 2, 3rd gen, 4th gen) Retina - iPad mini
-        return common.DensityResources.XXHIGH        
-    }  
+        return common.DensityResources.XXHIGH
+    }
 
     return common.DensityResources.HIGH
 }
